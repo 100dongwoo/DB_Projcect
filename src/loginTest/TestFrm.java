10fe9986d32b107766979fcb3a8ac5ad5b4dfca9
joshen <asdzxc250@naver.com>
2020-11-26 16:10:19 +0900

    Add login ui

---
+++ src/loginTest/TestFrm.java
@@ -0,0 +1,11 @@
+package loginTest;
+ 
+import javax.swing.JFrame;
+ 
+public class TestFrm extends JFrame{
+    public TestFrm() {
+        setSize(400, 400);
+        setVisible(true);
+    }
+}
+ 
