985f8b248328eaf3f385113355597df62b865695
joshen96 <asdzxc250@naver.com>
2020-12-01 19:22:23 +0900

    여러가지 수정

--- src/DBManager.java
+++ src/DBManager.java
@@ -42,49 +42,6 @@ public class DBManager {
         return result;
     }
 
-    public ArrayList<Rental> selectRental(String facilityName) {//프리페어먼트
-        ArrayList<Rental> rentals = new ArrayList<>();
-        String query = "SELECT " +
-                "대여내역.대여번호, " +
-                "대여내역.시작기간, " +
-                "대여내역.종료기간, " +
-                "대여내역.인원, " +
-                "대여내역.사유, " +
-                "대여내역.동의인, " +
-                "대여내역.건물, " +
-                "대여내역.호실, " +
-                "대여내역.허가자 " +
-                "from 대여내역, 시설물 " +
-                "where 대여내역.건물=시설물.건물번호 and 시설물.시설명=?";
-
-
-        try {
-            PreparedStatement pstmt = con.prepareStatement(query);
-            pstmt.setString(1, facilityName);
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                Rental rental = new Rental();
-                rental.setRentalNumber(rs.getInt(1));
-                rental.setStartPeriod(rs.getTimestamp(2));
-                rental.setEndPeriod(rs.getTimestamp(3));
-                rental.setPersonnel(rs.getInt(4));
-                rental.setReason(rs.getString(5));
-                rental.setDEUPerson(rs.getInt(6));
-                rental.setFacility(rs.getInt(7));
-                rental.setRoom(rs.getInt(8));
-                rental.setLicenser(rs.getInt(9));
-                rentals.add(rental);
-            }
-            rs.close();
-            pstmt.close();
-
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return rentals;
-    }
-
-
     public ArrayList<Rental> selectRental() {
         ArrayList<Rental> rentals = new ArrayList<>();
         try {
@@ -112,6 +69,47 @@ public class DBManager {
         }
         return rentals;
     }
+
+    public ArrayList<Rental> selectRental(String facilityName) {//프리페어먼트
+        ArrayList<Rental> rentals = new ArrayList<>();
+        String query = "SELECT " +
+                "대여내역.대여번호, " +
+                "대여내역.시작기간, " +
+                "대여내역.종료기간, " +
+                "대여내역.인원, " +
+                "대여내역.사유, " +
+                "대여내역.동의인, " +
+                "대여내역.건물, " +
+                "대여내역.호실, " +
+                "대여내역.허가자 " +
+                "from 대여내역, 시설물 " +
+                "where 대여내역.건물=시설물.건물번호 and 시설물.시설명=?";
+        try {
+            PreparedStatement pstmt = con.prepareStatement(query);
+            pstmt.setString(1, facilityName);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                Rental rental = new Rental();
+                rental.setRentalNumber(rs.getInt(1));
+                rental.setStartPeriod(rs.getTimestamp(2));
+                rental.setEndPeriod(rs.getTimestamp(3));
+                rental.setPersonnel(rs.getInt(4));
+                rental.setReason(rs.getString(5));
+                rental.setDEUPerson(rs.getInt(6));
+                rental.setFacility(rs.getInt(7));
+                rental.setRoom(rs.getInt(8));
+                rental.setLicenser(rs.getInt(9));
+                rentals.add(rental);
+            }
+            rs.close();
+            pstmt.close();
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return rentals;
+    }
+
     public boolean addRental(Rental rental) {
         boolean result = false;
         try {
